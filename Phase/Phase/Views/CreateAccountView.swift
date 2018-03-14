29cde7a53c199dbc51cbdb1b6fa8ba68013e01a9
Reiaz Gafar <reiazgafar@ac.c4q.nyc>
2018-03-14 16:30:18 -0400

    added views and view controllers

---
+++ Phase/Phase/Views/CreateAccountView.swift
@@ -0,0 +1,21 @@
+//
+//  CreateAccountView.swift
+//  Phase
+//
+//  Created by Reiaz Gafar on 3/14/18.
+//  Copyright © 2018 Reiaz Gafar. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
