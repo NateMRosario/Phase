7237e86bf84a0aeb04b51b628d1d88d9db70c092
Tyler Zhao <tylerzhao@ac.c4q.nyc>
2018-03-15 16:14:24 -0400

    add layout and discoverVC, transition to detail VC is broken, please fix

---
+++ Phase/Phase/ViewControllers/Discovery/DetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailViewController.swift
+//  Phase
+//
+//  Created by C4Q on 3/15/18.
+//  Copyright © 2018 Tyler Zhao. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBAction func tappedBackButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
