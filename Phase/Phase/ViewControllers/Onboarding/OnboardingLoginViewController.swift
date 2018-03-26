f0aeea7451ae4d99353a9acd1d10f6429f9fbde6
Tyler Zhao <tylerzhao@ac.c4q.nyc>
2018-03-26 15:26:52 -0400

    add blank notificationsVC files

--- Phase/Phase/ViewControllers/Onboarding/OnboardingLoginViewController.swift
+++ Phase/Phase/ViewControllers/Onboarding/OnboardingLoginViewController.swift
@@ -15,8 +15,6 @@ class OnboardingLoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
