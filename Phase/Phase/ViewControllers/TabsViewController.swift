38d8efbdcc37155b660e7346765284c258352b86
Tyler Zhao <tylerzhao@ac.c4q.nyc>
2018-03-28 18:21:09 -0400

    change buttons in profileVC

--- Phase/Phase/ViewControllers/TabsViewController.swift
+++ Phase/Phase/ViewControllers/TabsViewController.swift
@@ -21,29 +21,37 @@ class TabsViewController: UITabBarController {
                 
         // Profile VC
         let profileVC = ProfileViewController.instantiate(withStoryboard: "Main")
-        profileVC.tabBarItem = UITabBarItem()
-        profileVC.tabBarItem.title = "Profile"
-        
+        profileVC.tabBarItem.image = #imageLiteral(resourceName: "profile-unselected")
+        profileVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile-seleced")
+        profileVC.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+
         //Camera View Controller
         func newCollectionButtonPressed() {
             
         }
         
         let cameraVC = UINavigationController(rootViewController: CameraViewController())
-        cameraVC.tabBarItem = UITabBarItem(title: "Camera", image: #imageLiteral(resourceName: "camera"), selectedImage: #imageLiteral(resourceName: "camera"))
+        cameraVC.tabBarItem.image = #imageLiteral(resourceName: "camera-unseleced")
+        cameraVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "camera-selected")
+        cameraVC.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
         
         // Discover/Search VC
         let discoverVC = DiscoveryNavViewController(rootViewController: DiscoveryViewController.instantiate(withStoryboard: "Discover"))
-        discoverVC.tabBarItem.title = "Discover"
+        discoverVC.tabBarItem.image = #imageLiteral(resourceName: "icons8-search_filled 2")
+        discoverVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "icons8-search_filled")
+        discoverVC.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
         
         // HomeFeed VC
         let homeVC = UINavigationController(rootViewController: HomeViewController.instantiate(withStoryboard: "HomeFeed"))
-        homeVC.tabBarItem.title = "Home"
+        homeVC.tabBarItem.image = #imageLiteral(resourceName: "home-unselected")
+        homeVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "home-selected")
+        homeVC.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
         
         // Login VC
         let notificationVC = NotificationsViewController.instantiate(withStoryboard: "Notifications")
-        notificationVC.tabBarItem.title = "Notifications"
-        
+        notificationVC.tabBarItem.image = #imageLiteral(resourceName: "alert-unseleced")
+        notificationVC.tabBarItem.selectedImage = #imageLiteral(resourceName: "alert-selected")
+        notificationVC.tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
         self.setViewControllers([homeVC, discoverVC, cameraVC, notificationVC, profileVC], animated: true)
     }
 }
