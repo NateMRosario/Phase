d36ff49f6fdb60a3b206ac6214dacebdad5cf65d
Vikash Hart <vikashhart@gmail.com>
2018-03-29 16:55:00 -0400

    Confirm account view UI changes

--- Phase/Phase/ViewControllers/Auth/ConfirmAccountViewController.swift
+++ Phase/Phase/ViewControllers/Auth/ConfirmAccountViewController.swift
@@ -9,19 +9,32 @@
 import UIKit
 import AWSCognitoIdentityProvider
 import AWSCognitoAuth
+import Pastel
 
 
 class ConfirmAccountViewController: UIViewController {
 
     let confirmAccountView = ConfirmAccountView()
     var sentTo: String?
-    var user: AWSCognitoIdentityUser?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(confirmAccountView)
+        let pastelView = PastelView(frame: view.bounds)
+        
+        // Custom Direction
+        pastelView.startPastelPoint = .topLeft
+        pastelView.endPastelPoint = .bottomRight
+        
+        // Custom Duration
+        pastelView.animationDuration = 3.0
+        
+        // Custom Color
+        pastelView.setColors(HexStringToUIColor.appGradient)
+        
+        pastelView.startAnimation()
+        view.insertSubview(pastelView, at: 0)
         setupButtons()
-        print(user?.username ?? "no username")
     }
 
 
@@ -34,58 +47,11 @@ extension ConfirmAccountViewController {
     }
     
     @objc private func confirmButtonTapped() {
-        guard let confirmationCodeValue = self.confirmAccountView.confirmationCodeTextField.text, !confirmationCodeValue.isEmpty else {
-            let alertController = UIAlertController(title: "Confirmation code missing.",
-                                                    message: "Please enter a valid confirmation code.",
-                                                    preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-            alertController.addAction(okAction)
-            
-            self.present(alertController, animated: true, completion:  nil)
-            return
-        }
-        self.user?.confirmSignUp(self.confirmAccountView.confirmationCodeTextField.text!, forceAliasCreation: true).continueWith {[weak self] (task: AWSTask) -> AnyObject? in
-            guard let strongSelf = self else { return nil }
-            DispatchQueue.main.async(execute: {
-                if let error = task.error as NSError? {
-                    let alertController = UIAlertController(title: error.userInfo["__type"] as? String,
-                                                            message: error.userInfo["message"] as? String,
-                                                            preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-                    alertController.addAction(okAction)
-                    
-                    strongSelf.present(alertController, animated: true, completion:  nil)
-                } else {
-                    let _ = strongSelf.navigationController?.popToRootViewController(animated: true)
-                }
-            })
-            return nil
-        }
+
     }
     
     // handle code resend action
     @objc private func resendButtonTapped() {
-        self.user?.resendConfirmationCode().continueWith {[weak self] (task: AWSTask) -> AnyObject? in
-            guard let _ = self else { return nil }
-            DispatchQueue.main.async(execute: {
-                if let error = task.error as NSError? {
-                    let alertController = UIAlertController(title: error.userInfo["__type"] as? String,
-                                                            message: error.userInfo["message"] as? String,
-                                                            preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-                    alertController.addAction(okAction)
-                    
-                    self?.present(alertController, animated: true, completion:  nil)
-                } else if let result = task.result {
-                    let alertController = UIAlertController(title: "Code Resent",
-                                                            message: "Code resent to \(result.codeDeliveryDetails?.destination! ?? " no message")",
-                        preferredStyle: .alert)
-                    let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
-                    alertController.addAction(okAction)
-                    self?.present(alertController, animated: true, completion: nil)
-                }
-            })
-            return nil
-        }
+
     }
 }
