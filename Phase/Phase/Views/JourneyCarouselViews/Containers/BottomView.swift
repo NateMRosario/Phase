1b81cd077315646d04c6b3b07c5a6611ab362e0d
clint-steve-m <clint_m@clintmejia.com>
2018-04-02 12:30:12 -0400

    stashing

---
+++ Phase/Phase/Views/JourneyCarouselViews/Containers/BottomView.swift
@@ -0,0 +1,21 @@
+//
+//  BottomView.swift
+//  Phase
+//
+//  Created by Clint M on 4/2/18.
+//  Copyright © 2018 Reiaz Gafar. All rights reserved.
+//
+
+import UIKit
+
+class BottomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Phase/Phase/Views/JourneyCarouselViews/Containers/MiddleView.swift
@@ -0,0 +1,21 @@
+//
+//  MiddleView.swift
+//  Phase
+//
+//  Created by Clint M on 4/2/18.
+//  Copyright © 2018 Reiaz Gafar. All rights reserved.
+//
+
+import UIKit
+
+class MiddleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
